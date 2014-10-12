f451b24869c64027c4eed00684fe561664c69eeb
Ricardo Cerqueira <github@cerqueira.org>
2014-10-12 21:03:02 +0200

    Add WideVine blobs
    Change-Id: I8ca31fd0ecb2ce3f7e0bc8dff1b57d2c733cc725
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- mako/device-partial.mk
+++ mako/device-partial.mk
@@ -23,4 +23,9 @@ PRODUCT_COPY_FILES := \
     vendor/lge/mako/proprietary/keymaster.b02:system/vendor/firmware/keymaster/keymaster.b02:lge \
     vendor/lge/mako/proprietary/keymaster.b03:system/vendor/firmware/keymaster/keymaster.b03:lge \
     vendor/lge/mako/proprietary/keymaster.mdt:system/vendor/firmware/keymaster/keymaster.mdt:lge \
-
+    vendor/lge/mako/proprietary/libWVphoneAPI.so:system/lib/libWVphoneAPI.so \
+    vendor/lge/mako/proprietary/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
+    vendor/lge/mako/proprietary/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
+    vendor/lge/mako/proprietary/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
+    vendor/lge/mako/proprietary/libwvm.so:system/vendor/lib/libwvm.so \
+    vendor/lge/mako/proprietary/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
